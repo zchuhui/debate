ddf53cf2bab086d15532651bbfb22b29ef9e691d
Huige2012 <943339564@qq.com>
2017-02-28 00:13:41 +0800

    fuck

---
+++ app.js
@@ -0,0 +1,29 @@
+
+var koa = require('koa');
+var controller = require('koa-route');
+var app = koa(); 
+
+//页面渲染格式
+var view = require('co-views');
+var render = view('./view',{
+	map:{ html:'ejs' }
+});
+
+//静态资源
+var koa_static = require("koa-static-server");
+app.use(koa_static({
+	rootDir:'./static',  //所在目录
+	rootPath:'/static',  //访问地址
+	maxage:0,            //缓存
+}));
+
+//首页路由
+app.use(controller.get('/',function*(){
+	this.body = yield render('index',{title:'辩否首页'});
+}));
+
+
+
+
+app.listen(3005);
+console.log("start 3005!!");
